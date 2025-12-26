406b68e8de22f8fb1a54e41c52d68a66397d8e83
Dan Green <danngreen1@gmail.com>
2025-12-26 10:36:33 -0800

    Stubs for ptrs to unused models that are used by common code
    These are referred to in code that adds an expander from a right-click
    menu

--- metamodule/plugin.cpp
+++ metamodule/plugin.cpp
@@ -2,25 +2,28 @@
 
 Plugin *pluginInstance;
 
-__attribute__ ((visibility ("default")))
-void init(Plugin *p) {
-  pluginInstance = p;
+// Stubs for models that aren't in the MM plugin, but are used in common code
+Model *modelSapphireTricorder = nullptr;
+Model *modelSapphireChaops = nullptr;
 
-  p->addModel(modelSapphireElastika);
-  p->addModel(modelSapphireFrolic);
-  p->addModel(modelSapphireGalaxy);
-  p->addModel(modelSapphireGlee);
-  p->addModel(modelSapphireGravy);
-  p->addModel(modelSapphireHiss);
-  // p->addModel(modelSapphireMoots);
-  // p->addModel(modelSapphireNucleus);
-  // p->addModel(modelSapphirePivot);
-  // p->addModel(modelSapphirePolynucleus);
-  // p->addModel(modelSapphirePop);
-  // p->addModel(modelSapphireRotini);
-  // p->addModel(modelSapphireSam);
-  // p->addModel(modelSapphireTin);
-  // p->addModel(modelSapphireTout);
-  // p->addModel(modelSapphireTricorder);
-  // p->addModel(modelSapphireTubeUnit);
+__attribute__((visibility("default"))) void init(Plugin *p) {
+	pluginInstance = p;
+
+	p->addModel(modelSapphireElastika);
+	p->addModel(modelSapphireFrolic);
+	p->addModel(modelSapphireGalaxy);
+	p->addModel(modelSapphireGlee);
+	p->addModel(modelSapphireGravy);
+	p->addModel(modelSapphireHiss);
+	// p->addModel(modelSapphireMoots);
+	// p->addModel(modelSapphireNucleus);
+	// p->addModel(modelSapphirePivot);
+	// p->addModel(modelSapphirePolynucleus);
+	// p->addModel(modelSapphirePop);
+	// p->addModel(modelSapphireRotini);
+	// p->addModel(modelSapphireSam);
+	// p->addModel(modelSapphireTin);
+	// p->addModel(modelSapphireTout);
+	// p->addModel(modelSapphireTricorder);
+	// p->addModel(modelSapphireTubeUnit);
 }
